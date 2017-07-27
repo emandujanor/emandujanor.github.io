9c12e54f21d43ad1aad30360015f2f2cbbb06dbe
Edith Mandujano <edithmandujano001@gmail.com>
2017-07-27 13:37:54 -0500

    Mi Portafolio

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 $( document ).ready(function(){
    $(".button-collapse").sideNav();
    $('.carousel.carousel-slider').carousel({fullWidth: true});
-   setInterval(moverCarrusel, 5000); //
+   setInterval(moverCarrusel, 2000); //
    $('.materialboxed').materialbox();
 });
 
